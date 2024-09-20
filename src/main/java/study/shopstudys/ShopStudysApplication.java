7ea1c42bb9399759c074461c96fcaff33dba9788
유종화 <sola201@naver.com>
2024-09-20 16:08:13 +0900

    :building_construction: 프로젝트 초기 구조 세팅

---
+++ src/main/java/study/shopstudys/ShopStudysApplication.java
@@ -0,0 +1,13 @@
+package study.shopstudys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStudysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopStudysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/shopstudys/ShopStudysApplicationTests.java
@@ -0,0 +1,13 @@
+package study.shopstudys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopStudysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
